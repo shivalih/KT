aab39bee87b5a388c3f7c54d05297916c1a3c5e3
shivalih <shivalih@gmail.com>
2013-12-24 17:22:39 -0500

    Added KT login page

---
+++ KT/Controllers/KTLoginController.cs
@@ -0,0 +1,21 @@
+﻿using KT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KT.Controllers
+{
+    public class KTLoginController : Controller
+    {
+        public ActionResult KTLogin()
+        {
+            var model = new KTLoginModel();
+            model.Email = "Enter your email id here";
+            model.Password = "Enter Password";
+
+            return View(model);
+        }
+    }
+}
---
+++ KT/Models/KTLoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KT.Models
+{
+    public class KTLoginModel
+    {
+        public string Email{ get; set; }
+        public string Password{ get; set; }
+    }
+}
