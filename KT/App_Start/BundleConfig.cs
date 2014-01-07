16feedb7b13f445d10a547d2f792565b696948d9
shivalih <sprout9>
2014-01-07 15:24:12 -0500

    Button Logic

--- KT/App_Start/BundleConfig.cs
+++ KT/App_Start/BundleConfig.cs
@@ -24,6 +24,8 @@ namespace KT
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/KTcss").Include("~/Content/KTStyles.css"));
+            
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
